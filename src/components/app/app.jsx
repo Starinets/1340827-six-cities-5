5359320dad3ec64882eaacb6cdbf3bac169b1e80
Starinets <gobvoz@gmail.com>
2020-10-04 14:49:42 -0400

    routing added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
+import Login from './../login/login';
+import Favorites from './../favorites/favorites';
+import Offer from './../offer/offer';
 import Main from './../main/main';
 
 const App = (props) => {
+
   return (
-    <Main
-      {...props}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/">
+          <Offer />
+        </Route>
+        {/* не будем использовать 404, при любых не корректных данных отрисовать Main */}
+        <Route path="/">
+          <Main {...props} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
