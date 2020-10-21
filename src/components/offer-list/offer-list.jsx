987d4a6d1a9dca48e1f15f2471c58c1d3cb9a858
Starinets <gobvoz@gmail.com>
2020-10-21 13:08:05 -0400

    new component OfferList

---
+++ src/components/offer-list/offer-list.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as Type from '../../types';
+
+import OfferCard from './../offer-card/offer-card';
+
+const OfferList = (props) => {
+
+  const offers = props.offers.map((offer) => {
+    return (
+      <OfferCard
+        key = {offer.id}
+        {...offer}
+      />
+    );
+  });
+
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offers}
+    </div>
+  );
+};
+
+OfferList.propTypes = {
+  offers: Type.OFFERS.isRequired
+};
+
+export default OfferList;
