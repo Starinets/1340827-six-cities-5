b335b7afcb7358a1e201d06861b4da11da5ce81a
Starinets <gobvoz@gmail.com>
2020-10-26 18:48:07 -0400

    now Favorites component is dynamic

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import * as Type from '../../types';
+import {NavLink} from 'react-router-dom';
 
-const Favorites = () => {
+import OfferCard from './../offer-card/offer-card';
+
+const reduceOffersByCity = (offers) => {
+
+  return offers.reduce((offersByCity, offer) => {
+
+    if (Array.isArray(offersByCity[offer.city])) {
+      offersByCity[offer.city].push(offer);
+    } else {
+      offersByCity[offer.city] = [offer];
+    }
+
+    return offersByCity;
+  }, {});
+};
+
+const generateOfferList = (offers, onCardMouseEnter) => {
+  return offers.map((offer) => {
+    return (
+      <OfferCard
+        key = { offer.id }
+        offer = { offer }
+        onCardMouseEnter = { onCardMouseEnter }
+      />
+    );
+  });
+};
+
+const generateCityList = (cities, onCardMouseEnter) => {
+
+  return Object.entries(cities)
+    .map(([cityName, offers], index) => {
+      return (
+        <li className="favorites__locations-items" key={ index }>
+          <div className="favorites__locations locations locations--current">
+            <div className="locations__item">
+              <NavLink className="locations__item-link" to="/">
+                <span>{ cityName }</span>
+              </NavLink>
+            </div>
+          </div>
+          <div className="favorites__places">
+            { generateOfferList(offers, onCardMouseEnter) }
+          </div>
+        </li>
+      );
+    });
+};
+
+const Favorites = (props) => {
 
   return (
     <div className="page">
@@ -33,123 +83,9 @@ const Favorites = () => {
           <section className="favorites">
             <h1 className="favorites__title">Saved listing</h1>
             <ul className="favorites__list">
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <NavLink className="locations__item-link" to="/">
-                      <span>Amsterdam</span>
-                    </NavLink>
-                  </div>
-                </div>
-                <div className="favorites__places">
-                  <article className="favorites__card place-card">
-                    <div className="favorites__image-wrapper place-card__image-wrapper">
-                      <NavLink to="/">
-                        <img className="place-card__image" src="img/apartment-small-03.jpg" width="150" height="110" alt="Place image" />
-                      </NavLink>
-                    </div>
-                    <div className="favorites__card-info place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;180</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">In bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: `100%`}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <NavLink to="/">Nice, cozy, warm big bed apartment</NavLink>
-                      </h2>
-                      <p className="place-card__type">Apartment</p>
-                    </div>
-                  </article>
 
-                  <article className="favorites__card place-card">
-                    <div className="favorites__image-wrapper place-card__image-wrapper">
-                      <NavLink to="/">
-                        <img className="place-card__image" src="img/room-small.jpg" width="150" height="110" alt="Place image" />
-                      </NavLink>
-                    </div>
-                    <div className="favorites__card-info place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;80</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">In bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: `80%`}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <NavLink to="/">Wood and stone place</NavLink>
-                      </h2>
-                      <p className="place-card__type">Private room</p>
-                    </div>
-                  </article>
-                </div>
-              </li>
+              { generateCityList(reduceOffersByCity(props.offers), props.onCardMouseEnter) }
 
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <NavLink className="locations__item-link" to="/">
-                      <span>Cologne</span>
-                    </NavLink>
-                  </div>
-                </div>
-                <div className="favorites__places">
-                  <article className="favorites__card place-card">
-                    <div className="favorites__image-wrapper place-card__image-wrapper">
-                      <NavLink to="/">
-                        <img className="place-card__image" src="img/apartment-small-04.jpg" width="150" height="110" alt="Place image" />
-                      </NavLink>
-                    </div>
-                    <div className="favorites__card-info place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;180</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">In bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: `100%`}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <NavLink to="/">White castle</NavLink>
-                      </h2>
-                      <p className="place-card__type">Apartment</p>
-                    </div>
-                  </article>
-                </div>
-              </li>
             </ul>
           </section>
         </div>
@@ -161,6 +97,11 @@ const Favorites = () => {
       </footer>
     </div>
   );
+};
+
+Favorites.propTypes = {
+  offers: Type.OFFERS.isRequired,
+  onCardMouseEnter: Type.FUNCTION.isRequired
 };
 
 export default Favorites;
