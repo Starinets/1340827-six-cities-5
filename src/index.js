ca3c9f2e89edc96b8bbc4f3e9e16c56e309c7d13
Starinets <tsn.ipad@gmail.com>
2020-11-04 23:31:05 -0500

    New storage has been initialized. Provider used

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 import App from './components/app/app';
+
 import mock from './mocks/offers';
+import {reducer} from "./store/reducer";
 
 const MockState = {
   rentalOffersCount: 32,
   currentCity: `Amsterdam`
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      {...MockState}
-      offers={mock}
-    />,
+    <Provider store={store}>
+      <App
+        {...MockState}
+        offers={mock}
+      />
+    </Provider>,
     document.getElementById(`root`)
 );
