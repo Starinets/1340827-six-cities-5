653751feb5ea454774579fcf61053b60bd112c26
Starinets <tsn.ipad@gmail.com>
2020-11-13 16:06:08 -0500

    new HOC for the OfferList Component

---
+++ src/components/hocs/with-offer-list/with-offer-list.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {connect} from "react-redux";
+import {ActionCreator} from "../../../store/action";
+
+const withOfferList = (Component) => {
+  class WithOfferList extends React.PureComponent {
+    render() {
+      return (
+        <Component
+          {...this.props}
+        />
+      );
+    }
+  }
+
+  const mapDispatchToProps = (dispatch) => ({
+    changeHoveredOffer(hoveredOffer) {
+      dispatch(ActionCreator.setHoveredOffer(hoveredOffer));
+    }
+  });
+
+  return connect(null, mapDispatchToProps)(WithOfferList);
+};
+
+export default withOfferList;
