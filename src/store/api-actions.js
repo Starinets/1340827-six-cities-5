0a3318b53206a37ef463230e59db8f2c949b2812
Starinets <tsn.ipad@gmail.com>
2020-11-21 19:55:43 -0500

    added api-action

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,8 +1,39 @@
-import {loadOfferList} from './action';
+import {
+  loadOffersAction,
+  requiredAuthorizationAction,
+  setAuthInfo,
+  redirectToRoute
+} from "./action";
+import {
+  AuthorizationStatus,
+  APIRoute,
+  AppRoute
+} from "../const";
 
-const fetchOfferList = () => (dispatch, _getState, api) => (
-  api.get(`/hotels`)
-    .then(({data}) => dispatch(loadOfferList(data)))
+const fetchOffersList = () => (dispatch, _getState, api) => (
+  api.get(APIRoute.HOTELS)
+    .then(({data}) => dispatch(loadOffersAction(data)))
 );
 
-export {fetchOfferList};
+const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(APIRoute.LOGIN)
+    .then(() => dispatch(requiredAuthorizationAction(AuthorizationStatus.AUTH)))
+    .catch((err) => {
+      throw err;
+    })
+);
+
+const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(APIRoute.LOGIN, {email, password})
+    .then(({data}) => {
+      dispatch(setAuthInfo(data));
+      dispatch(requiredAuthorizationAction(AuthorizationStatus.AUTH));
+    })
+    .then(() => dispatch(redirectToRoute(AppRoute.ROOT)))
+);
+
+export {
+  fetchOffersList,
+  checkAuth,
+  login
+};
