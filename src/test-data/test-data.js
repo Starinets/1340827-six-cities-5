162349f2b8f6a6a245dfc8a573dd9e019b905040
Starinets <tsn.ipad@gmail.com>
2020-11-27 19:33:36 -0500

    test for FavoriteItems component

--- src/test-data/test-data.js
+++ src/test-data/test-data.js
@@ -26,3 +26,237 @@ export const AuthorizationStatus = {
   AUTH: `AUTH`,
   NO_AUTH: `NO_AUTH`,
 };
+
+export const Offers = [
+  {
+    "city": {
+      "name": `Cologne`,
+      "location": {
+        "latitude": 50.938361,
+        "longitude": 6.959974,
+        "zoom": 13
+      }
+    },
+    "image": `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/14.jpg`,
+    "images": [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/9.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/14.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/2.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/1.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/17.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/5.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`
+    ],
+    "name": `The house among olive `,
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 3.8,
+    "type": `house`,
+    "bedroomsCount": 3,
+    "adultsCount": 4,
+    "price": 493,
+    "features": [
+      `Laptop friendly workspace`,
+      `Washer`,
+      `Towels`,
+      `Fridge`,
+      `Air conditioning`,
+      `Baby seat`,
+      `Breakfast`
+    ],
+    "host": {
+      "id": 25,
+      "name": `Angelina`,
+      "isPro": true,
+      "avatar": `img/avatar-angelina.jpg`
+    },
+    "description": `Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.`,
+    "location": {
+      "latitude": 50.957361,
+      "longitude": 6.9509739999999995,
+      "zoom": 16
+    },
+    "id": 1
+  },
+  {
+    "city": {
+      "name": `Brussels`,
+      "location": {
+        "latitude": 50.846557,
+        "longitude": 4.351697,
+        "zoom": 13
+      }
+    },
+    "image": `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+    "images": [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/20.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/14.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/7.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/18.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/9.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`
+    ],
+    "name": `Amazing and Extremely Central Flat`,
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 4.3,
+    "type": `apartment`,
+    "bedroomsCount": 3,
+    "adultsCount": 8,
+    "price": 323,
+    "features": [
+      `Towels`,
+      `Baby seat`,
+      `Laptop friendly workspace`,
+      `Dishwasher`,
+      `Washing machine`,
+      `Air conditioning`,
+      `Washer`,
+      `Coffee machine`,
+      `Breakfast`,
+      `Fridge`
+    ],
+    "host": {
+      "id": 25,
+      "name": `Angelina`,
+      "isPro": true,
+      "avatar": `img/avatar-angelina.jpg`
+    },
+    "description": `This is a place for dreamers to reset, reflect, and create. Designed with a 'slow' pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.`,
+    "location": {
+      "latitude": 50.839557,
+      "longitude": 4.346697,
+      "zoom": 16
+    },
+    "id": 2
+  },
+  {
+    "city": {
+      "name": `Brussels`,
+      "location": {
+        "latitude": 50.846557,
+        "longitude": 4.351697,
+        "zoom": 13
+      }
+    },
+    "image": `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+    "images": [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/2.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/1.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/12.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/17.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/5.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/6.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/7.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`
+    ],
+    "name": `The house among olive `,
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 4.6,
+    "type": `apartment`,
+    "bedroomsCount": 4,
+    "adultsCount": 7,
+    "price": 218,
+    "features": [
+      `Washer`,
+      `Laptop friendly workspace`,
+      `Breakfast`,
+      `Air conditioning`,
+      `Baby seat`
+    ],
+    "host": {
+      "id": 25,
+      "name": `Angelina`,
+      "isPro": true,
+      "avatar": `img/avatar-angelina.jpg`
+    },
+    "description": `Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.`,
+    "location": {
+      "latitude": 50.865556999999995,
+      "longitude": 4.371696999999999,
+      "zoom": 16
+    },
+    "id": 3
+  },
+  {
+    "city": {
+      "name": `Amsterdam`,
+      "location": {
+        "latitude": 52.37454,
+        "longitude": 4.897976,
+        "zoom": 13
+      }
+    },
+    "image": `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+    "images": [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/6.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/17.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/14.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/18.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/12.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/5.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/2.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/1.jpg`
+    ],
+    "name": `Perfectly located Castro`,
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 4.6,
+    "type": `house`,
+    "bedroomsCount": 3,
+    "adultsCount": 7,
+    "price": 440,
+    "features": [
+      `Dishwasher`,
+      `Air conditioning`,
+      `Laptop friendly workspace`,
+      `Washer`,
+      `Baby seat`,
+      `Towels`,
+      `Breakfast`,
+      `Coffee machine`,
+      `Fridge`
+    ],
+    "host": {
+      "id": 25,
+      "name": `Angelina`,
+      "isPro": true,
+      "avatar": `img/avatar-angelina.jpg`
+    },
+    "description": `Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.`,
+    "location": {
+      "latitude": 52.385540000000006,
+      "longitude": 4.902976,
+      "zoom": 16
+    },
+    "id": 4
+  }
+];
